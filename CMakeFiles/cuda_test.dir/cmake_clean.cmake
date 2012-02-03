901b0fd8b6e7c06e3d7dcb678ac8284fc8450088
Teebone Ding <tbone.ding@gmail.com>
2012-02-03 18:50:01 +0800

    GPU test complete.

---
+++ CMakeFiles/cuda_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cuda_test.dir/./cuda_test_generated_cuda_test.cu.o"
+  "cuda_test.pdb"
+  "cuda_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/cuda_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
